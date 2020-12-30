31c014c62b2406e13b40cee6cf38bb8cee1f4dbd
Petro <p.such@nltu.lviv.ua>
2020-12-30 11:22:31 +0200

    push

---
+++ assets/AppAsset.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Main application asset bundle.
+ *
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/bootstrap.css',
+        'css/fontawesome-all.css',
+        'css/magnific-popup.css',
+        'css/swiper.css',
+        'css/styles.css',
+    ];
+    public $js = [
+        "js/jquery.min.js",
+        "js/popper.min.js",
+        "js/bootstrap.min.js",
+        "js/jquery.easing.min.js",
+        "js/swiper.min.js",
+        "js/jquery.magnific-popup.js",
+        "js/morphext.min.js",
+        "js/isotope.pkgd.min.js",
+        "js/validator.min.js",
+        "js/scripts.js"
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        //'yii\bootstrap\BootstrapAsset',
+    ];
+}
